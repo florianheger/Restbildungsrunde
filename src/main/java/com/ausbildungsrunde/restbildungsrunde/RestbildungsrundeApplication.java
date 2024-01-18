7624e6e9f6335811a2a91abe487c14e09a7da8e1
pgretschmann <pgretschmann@brockhaus-ag.de>
2024-01-18 15:35:07 +0100

    init

---
+++ src/main/java/com/ausbildungsrunde/restbildungsrunde/RestbildungsrundeApplication.java
@@ -0,0 +1,13 @@
+package com.ausbildungsrunde.restbildungsrunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbildungsrundeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbildungsrundeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ausbildungsrunde/restbildungsrunde/RestbildungsrundeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ausbildungsrunde.restbildungsrunde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbildungsrundeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
