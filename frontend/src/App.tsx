9d8f8a00697332e96e6c3b1d0c3827c31478141d
Danylo_Sichkar <126655174+boblmbq@users.noreply.github.com>
2024-01-18 17:44:53 +0100

    the fronted language was changed on TS insted JS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Frontend Part or Codewars-REST project
+        Fronted Part of Codewars-REST project
       </header>
     </div>
   );
