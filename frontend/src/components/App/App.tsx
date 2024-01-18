b74fb26ea7f22f12c67c797292ea1b5c9951d3b0
Danylo_Sichkar <126655174+boblmbq@users.noreply.github.com>
2024-01-18 18:01:59 +0100

    the minimal structure to the project was added

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./App.styled";
+
+function App() {
+  return (
+    <div>
+      <Header>Fronted Part of Codewars-REST project</Header>
+    </div>
+  );
+}
+
+export default App;
