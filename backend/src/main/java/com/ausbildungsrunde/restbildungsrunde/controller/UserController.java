987b3d46b17eb8cf53ee5109dee358535c97d020
fheger <fheger@brockhaus-ag.de>
2024-01-19 16:14:27 +0100

    add creatUser

--- backend/src/main/java/com/ausbildungsrunde/restbildungsrunde/controller/UserController.java
+++ backend/src/main/java/com/ausbildungsrunde/restbildungsrunde/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.ausbildungsrunde.restbildungsrunde.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ausbildungsrunde.restbildungsrunde.model.User;
+import com.ausbildungsrunde.restbildungsrunde.model.UserEntity;
+import com.ausbildungsrunde.restbildungsrunde.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class UserController {
+    private final UserRepository userRepository;
 
     @GetMapping("/hello")
     public String hello() {
@@ -14,5 +19,10 @@ public class UserController {
         return "Hello World!";
     }
 
-
+    @PostMapping("/user")
+    public void createUser(@RequestBody User user) {
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity userEntity = modelMapper.map(user, UserEntity.class);
+        userRepository.save(modelMapper.map(user, UserEntity.class));
+    }
 }
