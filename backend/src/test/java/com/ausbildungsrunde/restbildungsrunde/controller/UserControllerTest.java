63f56b3b6b1517367f99bbc90c4f8eae6a099ed8
fheger <fheger@brockhaus-ag.de>
2024-01-23 12:48:08 +0100

    add test for deleteUser

--- backend/src/test/java/com/ausbildungsrunde/restbildungsrunde/controller/UserControllerTest.java
+++ backend/src/test/java/com/ausbildungsrunde/restbildungsrunde/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.ausbildungsrunde.restbildungsrunde.controller;
 
 import com.ausbildungsrunde.restbildungsrunde.model.User;
 import com.ausbildungsrunde.restbildungsrunde.model.UserEntity;
+import com.ausbildungsrunde.restbildungsrunde.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,18 +12,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.net.URI;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class UserControllerTest {
     @Autowired
-    TestRestTemplate restTemplate;
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void createUser_ShouldCreateUser() {
         User newUser = new User();
-        newUser.setUsername("Test Testermann");
+        newUser.setUsername("Tom Testermann");
         newUser.setPoints(5);
 
         ResponseEntity<Void> response = restTemplate.postForEntity("/api/user", newUser, Void.class);
@@ -31,5 +40,20 @@ class UserControllerTest {
         assertThat(newUserEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(newUserEntity.getBody().getUsername()).isEqualTo(newUser.getUsername());
         assertThat(newUserEntity.getBody().getPoints()).isEqualTo(newUser.getPoints());
+    }
+
+    @Test
+    public void deleteUser_ShouldDeleteUser() {
+        UserEntity newUser = new UserEntity();
+        newUser.setUsername("Tom Testermann");
+        newUser.setPoints(5);
+
+        long id = userRepository.save(newUser).getId();
+
+        restTemplate.delete("/api/user/" + id);
+
+        Optional<UserEntity> result = userRepository.findById((int) id);
+
+        assertTrue(result.isEmpty());
     }
 }
