605c64b0f46c4ed553c570f459f8d737498e649c
fheger <fheger@brockhaus-ag.de>
2024-01-23 12:26:16 +0100

    add inmemory db for test

--- backend/src/test/java/com/ausbildungsrunde/restbildungsrunde/controller/UserControllerTest.java
+++ backend/src/test/java/com/ausbildungsrunde/restbildungsrunde/controller/UserControllerTest.java
@@ -8,11 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class UserControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
