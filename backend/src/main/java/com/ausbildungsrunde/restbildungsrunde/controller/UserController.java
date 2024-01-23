554ae0f8c72256efde15875940461b06ce59541c
fheger <fheger@brockhaus-ag.de>
2024-01-23 12:12:07 +0100

    add getUser

--- backend/src/main/java/com/ausbildungsrunde/restbildungsrunde/controller/UserController.java
+++ backend/src/main/java/com/ausbildungsrunde/restbildungsrunde/controller/UserController.java
@@ -5,8 +5,11 @@ import com.ausbildungsrunde.restbildungsrunde.model.UserEntity;
 import com.ausbildungsrunde.restbildungsrunde.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -16,9 +19,11 @@ public class UserController {
     private final UserRepository userRepository;
 
     @PostMapping("/user")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucb) {
         ModelMapper modelMapper = new ModelMapper();
-        userRepository.save(modelMapper.map(user, UserEntity.class));
+        UserEntity newUser = userRepository.save(modelMapper.map(user, UserEntity.class));
+        URI locationOfUser = ucb.path("api/user/{id}").buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(locationOfUser).build();
     }
 
     @DeleteMapping("/user")
@@ -34,5 +39,14 @@ public class UserController {
             userEntity.setPoints(points);
             userRepository.save(userEntity);
         }
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserEntity> getUser(@PathVariable long id) {
+        Optional<UserEntity> user = userRepository.findById((int)id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
